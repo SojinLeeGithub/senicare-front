8e6e3a1225c5fe2d1f0a00f3e686c76b652b6a50
Sojin <inailedit777@gmail.com>
2024-08-21 15:35:00 +0900

    test2

---
+++ src/Senicare.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Senicare.css';
+import Auth from 'src/views/Auth';
+
+export default function Senicare() {
+  return (
+    <>
+    <Auth />
+    </>
+  );
+}
+
