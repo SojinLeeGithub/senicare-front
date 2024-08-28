879a0fcbbd0fba869fed1c45cbedc4179cd8116d
Sojin <inailedit777@gmail.com>
2024-08-28 11:53:13 +0900

    2024-08-28 11:50

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import './style.css';
 
 interface Props {
-  label: string;
-  type: 'text' | 'password';
-  placeholder: string;
-  buttonName?: string;
+    label: string;
+    type: 'text' | 'password';
+    placeholder: string;
+    value: string;
+    message: string;
+    messageError: boolean;
+    buttonName?: string;
+
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+    onButtonClick?: () => void;
 }
 
-export default function InputBox({label, type, placeholder, buttonName }: Props) {
+export default function InputBox({ 
+    label, 
+    type, 
+    placeholder, 
+    value, 
+    buttonName,
+    message,
+    messageError,
+    onChange,
+    onButtonClick,
+}: Props) {
 
-  return (
-    <div className="input-box" >
-    <div className="label">{label}</div>
-    <div className="input-area">
-        <input type={type} placeholder={placeholder} />
-        {/* //buttonName 존재하면 뿌리고 존재 안하면 안뿌린다는 뜻 */}
-        {buttonName && <div className="input-button disable">{buttonName}</div>}
-        
-    </div>
-    <div className="message"></div>
-</div>
-  )
+    return (
+        <div className="input-box">
+            <div className="label">{label}</div>
+            <div className="input-area">
+                <input value={value} type={type} placeholder={placeholder} onChange={onChange} />
+                {buttonName && <div className={`input-button ${value ? 'active' : 'disable'}`} onClick={onButtonClick}>{buttonName}</div>}
+            </div>
+            <div className={`message ${messageError ? 'error' : 'primary'}`}>{message}</div>
+        </div>
+    )
+
 }
